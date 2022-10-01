b2c2ed019aaa9f304260849fec4ede91401464b3
Jorge <jandresgarcia513@gmail.com>
2022-10-01 04:15:40 -0400

    A8 code

--- src/components/tuiter/tuit-list/index.js
+++ src/components/tuiter/tuit-list/index.js
@@ -1,21 +1,34 @@
-import React from "react";
-import {useSelector} from "react-redux";
+import React, {useEffect, useState} from "react";
+import {useDispatch, useSelector}
+    from "react-redux";
+import {createTuit, findAllTuits}
+    from "../actions/tuits-actions.js";
 import TuitListItem
     from "./tuit-list-item";
+
 
 const TuitList = () => {
 
     const tuits = useSelector(
         state => state.tuits);
+    const dispatch = useDispatch();
+    useEffect(() =>
+            findAllTuits(dispatch),
+        []);
+
 
     return (
+        <>
         <ul className="ttr-tuits list-group">
             {
-                tuits.map && tuits.map(tuit =>
-                    <TuitListItem key={tuit._id}
-                                  tuit={tuit}/>)
+                    tuits.map && tuits.map(tuit => (
+                        <>
+                            <TuitListItem key={tuit._id} tuit={tuit}/>
+                        </>))
+
             }
         </ul>
+        </>
     );
 }
 
